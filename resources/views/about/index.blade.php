4020fc23085a04e304f52a7333bbd048c0b43c11
carlos paez <carlosa.paezc@autonoma.edu.co>
2023-09-29 22:10:42 -0500

    initial commit

---
+++ resources/views/about/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>QuoteServer es una base de datos publica de citas</h1>
+    <p>En QuoteServer puedes buscar citas de todo indole, desde frases celebres de la filosofia antigua lirica de la literatura moderna</p>
+</div>
+
+<div class="container">
+    <h2>Estos son los desarrolladores de QuoteServer!</h2>
+    <p>look at that pretty faces!</p>
+    <ul class="list-group">
+        <div class="row-5">
+            <li class="list-group-item">dev 1</li>
+            <li class="list-group-item">dev 2</li>
+            <li class="list-group-item">dev 3</li>
+            <li class="list-group-item">dev 4</li>
+            <li class="list-group-item">dev 10</li>
+        </div>
+        <div class="row-5">
+            <li class="list-group-item">dev 5</li>
+            <li class="list-group-item">dev 6</li>
+            <li class="list-group-item">dev 7</li>
+            <li class="list-group-item">dev 8</li>
+            <li class="list-group-item">dev 9</li>
+        </div>
+    </ul>
+</div>
+@endsection
